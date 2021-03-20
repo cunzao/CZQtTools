63585d485fab4d9becc9c8fd919f7bbd53fb9549
cunzao <cunzao@qq.com>
2021-03-20 10:26:10 +0800

    完善ImageViwer
    1. 重载了`setImg`方法，`setImg`可以传入`QImage`类型的图片数据了
    2. 添加无图片显示时的提示文字
    3. 添加设置提示文字功能，默认显示在左上角，在某些显示场景实用
    4. 添加设置`widget`的边框功能，例如无图片显示时，无边框看起来比较奇怪，故加此功能
    5. 增加图片是否为空的判断，避免图片为空继续使用图片导致的报错

--- ImageViwer/imageviewer.cpp
+++ ImageViwer/imageviewer.cpp
@@ -1,52 +1,105 @@
 ﻿#include "imageviewer.h"
 #include <QPainter>
+#include <QPaintEvent>
 
 ImageViewer::ImageViewer(QWidget *parent) : QWidget(parent)
   , m_bNeedCross(false)
+  , m_bNeedBorder(true)
 {
 }
 
-void ImageViewer::setImg(QString strImgPath)
+void ElectricAdjustableMirrorImageWidget::setImg(QString strImgPath)
 {
     m_img.load(strImgPath);
+//    this->update();
+    UpdateWidget();
 }
 
-void ImageViewer::setMark(QRect rectMark)
+void ElectricAdjustableMirrorImageWidget::setImg(QImage img)
+{
+    m_img = img;
+//    this->update();
+    UpdateWidget();
+}
+
+void ElectricAdjustableMirrorImageWidget::setImg(QByteArray baImg)
+{
+    m_img.loadFromData(baImg);
+//    this->update();
+    UpdateWidget();
+}
+
+void ElectricAdjustableMirrorImageWidget::setMark(QRect rectMark)
 {
     m_rectMarks.clear();
     m_rectMarks.push_back(rectMark);
 }
 
-void ImageViewer::setMarks(QVector<QRect> rectMarks)
+void ElectricAdjustableMirrorImageWidget::setMarks(QVector<QRect> rectMarks)
 {
     m_rectMarks.clear();
     m_rectMarks = rectMarks;
 }
 
-void ImageViewer::setNeedCross(bool bNeedCross)
+void ElectricAdjustableMirrorImageWidget::setNeedCross(bool bNeedCross)
 {
     m_bNeedCross = bNeedCross;
 }
 
-void ImageViewer::paintEvent(QPaintEvent *event)
+void ElectricAdjustableMirrorImageWidget::setNeedBorder(bool bNeedBorder)
+{
+    m_bNeedBorder = bNeedBorder;
+}
+
+void ElectricAdjustableMirrorImageWidget::setText(QString strToShow)
+{
+    m_strToShow = strToShow;
+    this->update();
+}
+
+void ElectricAdjustableMirrorImageWidget::sltUpdateWidget(double *pDoubleData)
+{
+    Q_UNUSED(pDoubleData);
+}
+
+void ElectricAdjustableMirrorImageWidget::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
+    QPainter painter(this);
+    // 绘制边框，当设置了需要边框而且未设置图片时绘制边框
+    if(m_bNeedBorder && m_img.isNull())
+    {
+        painter.drawRect(0, 0, this->width() - painter.pen().width(), this->height() - painter.pen().width());
+    }
     m_centerPoint = QPointF(this->width()/2.0, this->height()/2.0); // 计算中心点坐标
+
+    if(m_img.isNull()) // 如果没有设置照片则不绘制图片，转而显示提示
+    {
+        painter.drawText(m_centerPoint, "无信号！");
+        return;
+    }
     // Start 计算图片放缩比例
     float fScale, fWScale, fHScale;
     fWScale = this->width()/(m_img.width()*1.0);
     fHScale = this->height()/(m_img.height()*1.0);
     fScale = fHScale>fWScale?fWScale:fHScale;
     // End
+
+    // Start 放缩图片
     QImage scaledImg = m_img.scaled(m_img.width()*fScale, m_img.height()*fScale);
+    // End
+
     // Start 计算图片放置的起始位置
     int imgLocateY = (this->height() - scaledImg.height()) / 2;
     int imgLocateX = (this->width() - scaledImg.width()) / 2;
     //End
-    QPainter painter(this);
+
+    // Start 绘图
     painter.drawImage(imgLocateX > imgLocateY ? QPoint(imgLocateX, 0) : QPoint(0, imgLocateY),scaledImg);
+    // End
+
     painter.setPen(QColor(Qt::white));
-    if(!m_rectMarks.isEmpty())
+    if(!m_rectMarks.isEmpty()) // 判断是否需要显示标记框
     {
         for(auto mark : m_rectMarks)
         {
@@ -56,10 +109,24 @@ void ImageViewer::paintEvent(QPaintEvent *event)
             painter.drawRect(tmp);
         }
     }
-    if(m_bNeedCross)
+    if(m_bNeedCross) // 判断是否需要显示准星
     {
-        painter.drawLine(m_centerPoint.x(), m_centerPoint.y()-m_iCrossLength, m_centerPoint.x(), m_centerPoint.y()+m_iCrossLength);
-        painter.drawLine(m_centerPoint.x()-m_iCrossLength, m_centerPoint.y(), m_centerPoint.x()+m_iCrossLength, m_centerPoint.y());
-    }
 
+        painter.setPen(QPen(QBrush(Qt::white), 2));
+        painter.drawLine(m_centerPoint.x(), m_centerPoint.y()-m_iCrossLength*fScale, m_centerPoint.x(), m_centerPoint.y()+m_iCrossLength*fScale);
+        painter.drawLine(m_centerPoint.x()-m_iCrossLength*fScale, m_centerPoint.y(), m_centerPoint.x()+m_iCrossLength*fScale, m_centerPoint.y());
+    }
+    if(!m_strToShow.isEmpty()) // 判断是否需要显示文字
+    {
+        painter.setPen(Qt::white);
+        int iMargin = 50; // 边距，距离边框的距离
+        painter.drawText(imgLocateX > imgLocateY ? QPoint(imgLocateX + iMargin*fScale, iMargin*fScale) : QPoint(iMargin*fScale, imgLocateY + iMargin*fScale), m_strToShow);
+    }
+}
+
+void ElectricAdjustableMirrorImageWidget::UpdateWidget()
+{
+    m_rectMarks.clear();
+    m_strToShow.clear();
+    this->update();
 }
--- ImageViwer/imageviewer.h
+++ ImageViwer/imageviewer.h
@@ -1,6 +1,10 @@
 ﻿#ifndef IMAGEVIEWER_H
 #define IMAGEVIEWER_H
 
+#ifndef IMAGEVIEWER_VERSION
+#define IMAGEVIEWER_VERSION 1.1
+#endif
+
 #include <QWidget>
 
 class ImageViewer : public QWidget
@@ -9,9 +13,13 @@ class ImageViewer : public QWidget
 public:
     explicit ImageViewer(QWidget *parent = nullptr);
     void setImg(QString strImgPath); // 设置显示的图片，输入参数为图片路径
+    void setImg(QImage img); // 设置图片，输入参数位图片数据
+    void setImg(QByteArray baImg); // 设置图片，输入参数为图片的字节数组
     void setMark(QRect rectMark); // 设置标记框显示的位置
     void setMarks(QVector<QRect> rectMarks); // 设置多个标记框
     void setNeedCross(bool bNeedCross); // 设置是否需要十字架
+    void setNeedBorder(bool bNeedBorder); // 设置是否需要边框
+    void setText(QString strToShow); // 设置需要显示的文字
 
 signals:
 
@@ -22,8 +30,12 @@ private:
     QImage m_img; // 存储需要显示的图片
     QVector<QRect> m_rectMarks; // 存储标记框的位置
     bool m_bNeedCross; // 是否需要十字架
+    bool m_bNeedBorder; // 是否需要显示边框
     QPointF m_centerPoint; // 中心点坐标
-    int m_iCrossLength = 10; // 十字架的长度
+    int m_iCrossLength = 20; // 十字架的长度
+    QString m_strToShow; // 需要进行显示文字
+
+    void UpdateWidget();
 };
 
 #endif // IMAGEVIEWER_H
